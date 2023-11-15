62834605db4a7f70656126c6842324c3aec0cacb
Nahuel Campo <nahuel9567@gmail.com>
2023-11-15 14:36:42 -0300

    ProvinceRepository created

---
+++ src/main/java/semillero/ecosistema/repository/ProvinceRepository.java
@@ -0,0 +1,15 @@
+package semillero.ecosistema.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import semillero.ecosistema.entity.CountryEntity;
+import semillero.ecosistema.entity.ProvinceEntity;
+
+import java.util.List;
+
+@Repository
+public interface ProvinceRepository extends JpaRepository<ProvinceEntity, Long> {
+    ProvinceEntity findByNombre (String nombre);
+
+    List<ProvinceEntity> findByPais(CountryEntity pais);
+}
