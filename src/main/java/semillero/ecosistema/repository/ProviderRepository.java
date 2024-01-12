5366a23e252e60d0acb2d379cbcbecf081f4724d
nzaeta <nicocpla@hotmail.com>
2024-01-12 14:26:09 -0300

    corregido prov aceptados repositorio

--- src/main/java/semillero/ecosistema/repository/ProviderRepository.java
+++ src/main/java/semillero/ecosistema/repository/ProviderRepository.java
@@ -14,19 +14,19 @@ public interface ProviderRepository extends JpaRepository<ProviderEntity, String
     List<ProviderEntity> findByName(String name);
     List<ProviderEntity> findByStatus(String status);
 
-    @Query("SELECT a FROM ProviderEntity a WHERE a.name LIKE %:nombre% and a.deleted = false")
+    @Query("SELECT a FROM ProviderEntity a WHERE a.name LIKE %:nombre% and a.deleted = false and status = 'ACEPTADO'")
     public List<ProviderEntity> searchProviderByName(@Param("nombre") String nombre);
 
     @Query("SELECT a FROM ProviderEntity a WHERE a.deleted = false and status = 'ACEPTADO'")
     public List<ProviderEntity> listarAceptados();
 
-    @Query("SELECT a FROM ProviderEntity a WHERE a.deleted = false and a.category.nombre = :categoria")
+    @Query("SELECT a FROM ProviderEntity a WHERE a.deleted = false and a.category.nombre = :categoria and status = 'ACEPTADO'")
     public List<ProviderEntity> listarPorCategoria(@Param("categoria") String categoria);
 
     @Query(value = "SELECT * FROM proveedores p WHERE (p.status = :REVISION_INICIAL OR p.status = :CAMBIOS_REALIZADOS) and p.deleted = false", nativeQuery = true)
     List<ProviderEntity> findAllByStatus(String REVISION_INICIAL, String CAMBIOS_REALIZADOS);
 
-    @Query("SELECT p FROM ProviderEntity p WHERE (p.country.name = :pais and p.province.name = :provincia and p.city = :ciudad) and p.deleted = false")
+    @Query("SELECT p FROM ProviderEntity p WHERE (p.country.name = :pais and p.province.name = :provincia and p.city = :ciudad) and p.deleted = false and status = 'ACEPTADO'")
     List<ProviderEntity> findByLocation(String pais, String provincia, String ciudad);
 
     @Query("SELECT a FROM ProviderEntity a WHERE a.deleted = false and a.user.email = :usuario")
